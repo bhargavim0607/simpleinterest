a9991242990a67a0b3dbc0f0ad32010c636947f0
bhargavim0607 <68108779+bhargavim0607@users.noreply.github.com>
2021-05-04 14:51:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,17 +8,19 @@ var t,p,r;
     p=document.getElementById("p").value;
     r=document.getElementById("r").value;
     if(tt!=tr){
-        tt=convert(tt);
-        tr=convert(tr);
+        if(tt=='m' && tr=='y'){
+            t=t/12;
+        }
+        if(tr=='m' && tt=='y'){
+            r=r*12
+        }
+       
+        console.log(t,r);
     }    
     var SI=Math.ceil(p*t*r/100);
     var a=new Number(p)+new Number(SI);
-        document.getElementById("si").innerHTML="The Interest Is:"+SI;
-        document.getElementById("ci").innerHTML="The Amount Is:"+a;
+        document.getElementById("si").innerHTML="The Interest Is:$"+SI;
+        document.getElementById("ci").innerHTML="The Amount Is:$"+a;
     console.log(tt,tr,p,t,r);
  }
- function convert(i){
-     if(i=='m'){
-         return i/12;
-     }
- }
+ 
